9a379180111319e21bcf803afe37e1d0979116d1
Karan Galgat <kgalgat117@gmail.com>
2022-08-06 17:28:29 +0530

    Create linkedIn_Auto_Connection_Add.js

---
+++ linkedIn_Auto_Connection_Add.js
@@ -0,0 +1,35 @@
+let counter = 0;
+let interval = setInterval(() => {
+    console.log("Scrolling", counter);
+    window.scrollTo(0, document.body.scrollHeight);
+    counter++;
+}, 3000);
+
+setTimeout(() => {
+    clearInterval(interval);
+    console.log("interval cleared")
+    let footers = document.getElementsByClassName("ph3 pb3");
+    console.log("total data", footers.length);
+    let connections = [];
+    for (let footer of footers) {
+        let button = footer.getElementsByTagName("button").length ? footer.getElementsByTagName("button")[0] : null;
+
+        if (button) {
+            if (button.outerHTML.includes("Connect")) {
+                connections.push(button);
+            }
+        }
+    }
+    console.log("total connections", connections.length);
+
+    let start = 0;
+    for (let connection of connections) {
+        setTimeout(() => {
+            connection.click();
+            setTimeout(() => {
+                document.querySelectorAll('[aria-label="Send now"]')[0].click();
+            }, 200);
+        }, start);
+        start += 3200;
+    }
+}, 30000)
